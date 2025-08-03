e94721915044dca720360bf94caf1933b85789f0
juangabrielsv <juangabrielsv@gmail.com>
2025-08-03 02:35:38 +0200

    Configurado perfiles

---
+++ indievivo-backend/src/main/java/com/santech/indievivobackend/IndievivoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.santech.indievivobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndievivoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndievivoBackendApplication.class, args);
+	}
+
+}
---
+++ indievivo-backend/src/test/java/com/santech/indievivobackend/IndievivoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santech.indievivobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndievivoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
