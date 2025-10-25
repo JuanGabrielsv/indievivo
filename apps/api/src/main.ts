0cdfe07d5acd6cd9a88795804759b457e1a3cdaa
juangabrielsv <juangabrielsv@gmail.com>
2025-10-25 18:57:41 +0200

    Añadir validaciones globales utilizando ValidationPipe para mejorar la seguridad y la integridad de los datos en la aplicación.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilita las validaciones globalmente
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina propiedades que no están en el DTO
+      forbidNonWhitelisted: true, // Lanza error si envían propiedades no permitidas
+      transform: true, // Transforma los payloads a instancias de los DTOs
+    })
+  )
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
